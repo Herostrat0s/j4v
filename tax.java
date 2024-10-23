1d53bfe94aa8c3128669b05e2ad4dc52b5055051
Herostrat0s <muhammed.arslan16@ogr.sakarya.edu.tr>
2024-10-23 17:07:34 +0300

    fixed something on tax rate

--- tax.java
+++ tax.java
@@ -8,10 +8,18 @@ public class tax {
         System.out.println("What is the price of product:");
         int pre_tax_price = input.nextInt();
 
+        if (pre_tax_price > 1000) {
         double after_tax_price = pre_tax_price + pre_tax_price * 0.18;
-        System.out.println("The after tax price is: " + after_tax_price);
-
+        System.out.println("The after tax price is: " +String.format("%.1f", after_tax_price));
         double tax_price = pre_tax_price * 0.18f;
         System.out.println("The tax added is: " +String.format("%.1f", tax_price));
+        }
+
+        else { 
+        double after_tax_price = pre_tax_price + pre_tax_price * 0.08;
+        System.out.println("The after tax price is: " +String.format("%.1f", after_tax_price));
+        double tax_price = pre_tax_price * 0.08f;
+        System.out.println("The tax added is: " +String.format("%.1f", tax_price));
+        }
     }
 }
